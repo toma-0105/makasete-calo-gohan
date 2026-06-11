574d571c52e244ebe1b18f205b2fcf14a99c11fc
toma <emiriahahaha@gmail.com>
2026-06-11 17:21:46 +0900

    feat: マイページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  get "mypage", to: "mypages#show", as: :mypage
+
   root "static_pages#top"
 end
---
+++ spec/requests/mypages_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Mypages", type: :request do
+  describe "GET /mypage" do
+    context "ログインしている場合" do
+      let(:user) { create(:user, name: "テストユーザー", email: "test@example.com") }
+
+      before do
+        sign_in user
+        get mypage_path
+      end
+
+      it "200 OKを返す" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "ログインユーザーの名前が表示される" do
+        expect(response.body).to include("テストユーザー")
+      end
+
+      it "ログインユーザーのメールアドレスが表示される" do
+        expect(response.body).to include("test@example.com")
+      end
+    end
+
+    context "ログインしていない場合" do
+      before { get mypage_path }
+
+      it "ログインページにリダイレクトされる" do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it "302 Foundを返す" do
+        expect(response).to have_http_status(:found)
+      end
+    end
+  end
+end
