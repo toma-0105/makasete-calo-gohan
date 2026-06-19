af10fbebea53740a221b6fb17d4096c85744559e
toma <emiriahahaha@gmail.com>
2026-06-19 10:06:10 +0900

    test: UserAllergenモデルのバリデーションテスト追加

--- app/models/user_allergen.rb
+++ app/models/user_allergen.rb
@@ -1,4 +1,7 @@
 class UserAllergen < ApplicationRecord
   belongs_to :user
   belongs_to :allergen_master
+
+  validates :user_id, :allergen_master_id, presence: true
+  validates :user_id, uniqueness: { scope: :allergen_master_id }
 end
--- spec/factories/user_allergens.rb
+++ spec/factories/user_allergens.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   factory :user_allergen do
+    association :user
+    association :allergen_master
   end
 end
