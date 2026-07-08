7049cb6b0660a6b39dc4cb3825330b58be6a0ebe
toma <emiriahahaha@gmail.com>
2026-07-08 21:19:45 +0900

    tdee_profiles_spec.rbテスト追加

--- spec/requests/tdee_profiles_spec.rb
+++ spec/requests/tdee_profiles_spec.rb
@@ -10,4 +10,26 @@ RSpec.describe "TdeeProfiles", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET /tdee_profiles/:id（診断結果画面）" do
+    let(:tdee_profile) { create(:tdee_profile, user: user, tdee: 2500) }
+
+    before do
+      sign_in user
+      get tdee_profile_path(tdee_profile)
+    end
+
+    it "200 OKを返す" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "計算されたTDEEが表示される" do
+      expect(response.body).to include("2,500")
+    end
+
+    it "献立を生成するボタンが表示される" do
+      expect(response.body).to include("献立を生成する")
+      expect(response.body).to include(menus_path)
+    end
+  end
 end
