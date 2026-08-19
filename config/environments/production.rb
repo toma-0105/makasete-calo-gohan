e9903f5e152d0b6715f32f295b50bee6a946837a
toma <emiriahahaha@gmail.com>
2026-08-19 23:16:40 +0900

    feat: メール送信基盤（Resend・good_job・letter_opener_web）を整備する

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :resend
+  config.action_mailer.raise_delivery_errors = true
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "makasete-calo-gohan.onrender.com", protocol: "https" }
