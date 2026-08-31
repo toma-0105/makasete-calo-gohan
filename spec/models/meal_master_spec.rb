742cc0ea184b17ad5b826f919bdfe12498227064
toma <emiriahahaha@gmail.com>
2026-08-31 23:57:36 +0900

    test: 材料・手順のバリデーションテストを追加する

--- spec/models/meal_master_spec.rb
+++ spec/models/meal_master_spec.rb
@@ -51,5 +51,71 @@ RSpec.describe MealMaster, type: :model do
         expect(meal_master).not_to be_valid
       end
     end
+        describe "ingredients（材料）のバリデーション" do
+      context "未設定（空配列）の場合" do
+        it "有効になる" do
+          meal_master.ingredients = []
+          expect(meal_master).to be_valid
+        end
+      end
+
+      context "配列以外が保存された場合" do
+        it "無効になる" do
+          meal_master.ingredients = "鶏むね肉100g"
+          expect(meal_master).not_to be_valid
+        end
+      end
+
+      context "nameが欠けている材料がある場合" do
+        it "無効になる" do
+          meal_master.ingredients = [ { "name" => "", "amount" => "100g" } ]
+          expect(meal_master).not_to be_valid
+        end
+      end
+
+      context "amountが欠けている材料がある場合" do
+        it "無効になる" do
+          meal_master.ingredients = [ { "name" => "鶏むね肉", "amount" => "" } ]
+          expect(meal_master).not_to be_valid
+        end
+      end
+
+      context "name・amountが揃っている場合" do
+        it "有効になる" do
+          meal_master.ingredients = [ { "name" => "鶏むね肉", "amount" => "100g" } ]
+          expect(meal_master).to be_valid
+        end
+      end
+    end
+
+    describe "steps（手順）のバリデーション" do
+      context "未設定（空配列）の場合" do
+        it "有効になる" do
+          meal_master.steps = []
+          expect(meal_master).to be_valid
+        end
+      end
+
+      context "配列以外が保存された場合" do
+        it "無効になる" do
+          meal_master.steps = "フライパンで焼く"
+          expect(meal_master).not_to be_valid
+        end
+      end
+
+      context "空の手順が含まれる場合" do
+        it "無効になる" do
+          meal_master.steps = [ "鶏肉を切る", "" ]
+          expect(meal_master).not_to be_valid
+        end
+      end
+
+      context "すべての手順に内容がある場合" do
+        it "有効になる" do
+          meal_master.steps = [ "鶏肉を切る", "焼く" ]
+          expect(meal_master).to be_valid
+        end
+      end
+    end
   end
 end
