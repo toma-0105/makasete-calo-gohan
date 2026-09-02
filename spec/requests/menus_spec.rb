74b5ba4532497b8f63ba9deb2a0398e81d6533e1
toma <emiriahahaha@gmail.com>
2026-09-02 21:29:04 +0900

    feat: 献立詳細画面にPFC・材料・調理手順・アレルギー表示を追加する

--- spec/requests/menus_spec.rb
+++ spec/requests/menus_spec.rb
@@ -117,4 +117,104 @@ RSpec.describe "Menus", type: :request do
       end
     end
   end
+
+    describe "GET /menus/:id（献立詳細）" do
+      context "ログインしている場合" do
+        let(:user) { create(:user) }
+        let(:menu) { create(:menu, user: user) }
+
+        before { sign_in user }
+
+        it "200 OKを返す" do
+          get menu_path(menu)
+          expect(response).to have_http_status(:ok)
+        end
+
+        context "料理にレシピ情報(材料・手順・PFC)がある場合" do
+          let(:allergen) { create(:allergen_master, name: "鶏肉") }
+          let(:meal_master) do
+            create(:meal_master,
+                  name: "鶏むね肉のレモン塩麹グリル(100g)",
+                  protein: 24, fat: 4, carbohydrate: 2,
+                  ingredients: [ { "name" => "鶏むね肉", "amount" => "100g" } ],
+                  steps: [ "鶏むね肉を焼く" ])
+          end
+
+          before do
+            create(:meal_ingredient, meal_master: meal_master, allergen_master: allergen)
+            create(:meal, menu: menu, meal_master: meal_master, meal_timing: :breakfast)
+          end
+
+          it "「レシピを見る」トグルが表示される" do
+            get menu_path(menu)
+            expect(response.body).to include("レシピを見る")
+          end
+
+          it "材料が表示される" do
+            get menu_path(menu)
+            expect(response.body).to include("鶏むね肉")
+          end
+
+          it "PFCの内訳が表示される" do
+            get menu_path(menu)
+            expect(response.body).to include("たんぱく質24g")
+          end
+
+          it "アレルギー表示がされる" do
+            get menu_path(menu)
+            expect(response.body).to include("アレルギー表示")
+            expect(response.body).to include("鶏肉")
+          end
+        end
+
+        context "料理にレシピ情報が無い場合" do
+          let(:meal_master) { create(:meal_master) }
+
+          before { create(:meal, menu: menu, meal_master: meal_master, meal_timing: :breakfast) }
+
+          it "「レシピを見る」トグルが表示されない" do
+            get menu_path(menu)
+            expect(response.body).not_to include("レシピを見る")
+          end
+        end
+
+        context "アレルゲンはあるがレシピ情報が無い場合（既存データを想定）" do
+          let(:allergen) { create(:allergen_master, name: "小麦") }
+          let(:meal_master) { create(:meal_master) }
+
+          before do
+            create(:meal_ingredient, meal_master: meal_master, allergen_master: allergen)
+            create(:meal, menu: menu, meal_master: meal_master, meal_timing: :breakfast)
+          end
+
+          it "アレルギー表示はされる" do
+            get menu_path(menu)
+            expect(response.body).to include("アレルギー表示")
+            expect(response.body).to include("小麦")
+          end
+
+          it "「レシピを見る」トグルは表示されない" do
+            get menu_path(menu)
+            expect(response.body).not_to include("レシピを見る")
+          end
+        end
+      end
+
+      context "他人の献立の場合" do
+        it "404になる" do
+          other_menu = create(:menu)
+          sign_in create(:user)
+          get menu_path(other_menu)
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+
+      context "ログインしていない場合" do
+        it "ログインページにリダイレクトされる" do
+          menu = create(:menu)
+          get menu_path(menu)
+          expect(response).to redirect_to(new_user_session_path)
+        end
+      end
+    end
 end
